13d4c625958e624730e9702d7ea7b4d34416deb4
LoveSnowEx <lsktw22@gmail.com>
2024-12-31 13:32:07 +0800

    Add layout component for page structure

---
+++ routes/_layout.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "$fresh/server.ts";
+
+export default function Layout({ Component, state }: PageProps) {
+  return (
+    <div class="flex flex-col min-h-dvh">
+      <div class="flex-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto">
+        <Component />
+      </div>
+    </div>
+  );
+}
