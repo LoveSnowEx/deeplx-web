a5b8c2196968c42f7e0b1c32e7b0a070b98900e1
LoveSnowEx <lsktw22@gmail.com>
2025-01-04 17:50:48 +0800

    Remove unused state parameter from Layout component

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,6 +1,6 @@
 import { PageProps } from "$fresh/server.ts";
 
-export default function Layout({ Component, state }: PageProps) {
+export default function Layout({ Component }: PageProps) {
   return (
     <div class="flex flex-col min-h-dvh bg-gray-100">
       <div class="flex-1 flex flex-col justify-center items-center w-full max-w-screen-xl mx-auto">
