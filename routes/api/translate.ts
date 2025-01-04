72b3a32433a938751dccea2fd4d39ff698b53ef7
LoveSnowEx <lsktw22@gmail.com>
2025-01-04 23:52:03 +0800

    Refactor translation API URL formatting in translate handler

--- routes/api/translate.ts
+++ routes/api/translate.ts
@@ -1,16 +1,15 @@
 import { Handlers } from "$fresh/server.ts";
 
-const TRANSLATION_API_URL = 'https://deeplx.mingming.dev/translate';
+const TRANSLATION_API_URL = "https://deeplx.mingming.dev/translate";
 
 export const handler: Handlers = {
-    async POST(req, _ctx) {
-      const response = await fetch(TRANSLATION_API_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(await req.json()),
-      });
-      return new Response(JSON.stringify(await response.json()));
-    },
-  };
+  async POST(req, _ctx) {
+    return await fetch(TRANSLATION_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(await req.json()),
+    });
+  },
+};
